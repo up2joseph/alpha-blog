a09b301345bc90f1cbf4648ec31439c381e458c6
joseph thomas <up2josephthomas@gmail.com>
2020-08-03 19:02:55 +0530

    re db second

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_124728) do
+ActiveRecord::Schema.define(version: 2020_08_03_130943) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
