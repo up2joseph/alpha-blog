b7ed07f50d1a9802310c60d8153a91cfe3066a8d
joseph thomas <up2josephthomas@gmail.com>
2020-08-03 18:29:09 +0530

    re db

--- db/migrate/20200803124728_create_articles.rb
+++ db/migrate/20200803124728_create_articles.rb
@@ -1,6 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles do |t|
+     create_table :articles do |t|
     	t.string :title
     	t.text :description
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_095434) do
+ActiveRecord::Schema.define(version: 2020_08_03_124728) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
